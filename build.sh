05be3cebe94aace69335d4c8400b2cec572c864e
Alexey Duryagin <aiduryagih@gmail.com>
2024-10-21 16:14:59 +0300

    feat: build script

---
+++ build.sh
@@ -0,0 +1,3 @@
+skip export
+cp .build/skip-export/*-release* ../Android/lib/release/ && echo "Release files successfully copied to ../Android/lib/release/"
+cp .build/skip-export/*-debug* ../Android/lib/debug/ && echo "Debug files successfully copied to ../Android/lib/debug/"
